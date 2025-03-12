2e4ad5804f2eb131a863a2d24f7cf32a7594d731
gbaduqui <baduquig@gmail.com>
2025-03-11 20:51:24 -0700

    Develop mysql connector methods

---
+++ mysql_db.py
@@ -0,0 +1,23 @@
+import mysql.connector
+import mysql_connection_string
+
+def execute_proc(sql: str) -> str:
+    conn = mysql.connector.connect(**mysql_connection_string.config)
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    status = cursor.fetchone()[0]
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return status
+
+
+def call_view(sql: str) -> list:
+    conn = mysql.connector.connect(**mysql_connection_string.config)
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return results
