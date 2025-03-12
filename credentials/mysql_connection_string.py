4fe0405f2da04283b10e43a0f820a08c76d07513
gbaduqui <baduquig@gmail.com>
2025-03-12 15:33:42 -0700

    ignore mysql_connection_string file

---
+++ credentials/mysql_connection_string.py
@@ -0,0 +1,9 @@
+# Supply connection string parameters for your MySQL database instance
+# Copy and/or rename file, removing "sample_" prefix
+config = {
+    'user': 'root',
+    'password': 'C-Townpack2013',
+    'host': 'localhost',
+    'port': 3306,
+    'database': 'pickem_db',
+}
