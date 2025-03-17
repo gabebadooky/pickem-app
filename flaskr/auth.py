b299fe90b11e071f3e0956f1f13e6f78369e6c93
gabebadooky <baduquig@gmail.com>
2025-03-16 20:58:40 -0700

    develop update-email and update-phone endpoints and methods

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -7,18 +7,16 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.post('/register')
 def register():
     data = request.json
-    procedure_status = create_user(data)
-    return jsonify({'status': procedure_status})
+    return create_user(data)
 
 @bp.post('/login')
 def login():
     data = request.json
-    response = create_user(data)
-    return jsonify(response)
-
+    return get_user(data)
+    
 @bp.before_app_request
 def load_user():
-    user_id = session.get('user_id')
+    user_id = session.get('userID')
     if user_id is None:
         g.user = None
     else:
@@ -31,17 +29,16 @@ def logout():
 
 
 
-def get_user(data) -> dict:
+def get_user(data) -> tuple:
     try:
-        user = mysql_db.get_user_by_username(data['username'])
-    
+        user = mysql_db.get_user_by_username(data['username'])  
         if user is None:
             response_status = jsonify({"error": "Not Found", "message": "No users found associated to the provided username."}), 406
         elif check_password_hash(user['password'], data['password']):
             response_status = ({"error": "Incorrect username or password", "message": "Incorrect Username or Password"}), 406
         else:
             session.clear()
-            session['user_id'] = user['user_id']
+            session['user_id'] = user['userID']
             response_status = jsonify({
                 'username': user['username'],
                 'favoriteTeam': user['favoriteTeam'],
@@ -49,30 +46,32 @@ def get_user(data) -> dict:
                 'emailAddress': user['email_address'],
                 'phone': user['phone']
             }), 200
-
     except Exception as e:
         response_status = jsonify({"error": "Request Error", "message": f"{e}"}), 400
     return response_status
 
 
-def create_user(data) -> str:
+def create_user(data) -> tuple:
     try:
-        sql_statement = (f"""CALL PROC_CREATE_USER(
-                            '{data["username"]}',
-                            '{generate_password_hash(data["password"])}', 
-                            '{data["favorite_team"]}', 
-                            '{data["notification_preference"]}', 
-                            '{data["email_address"]}', 
-                            '{data["phone_number"]}', 
-                            @status);
-                        """)
+        sql_statement = conncatenate_create_user_sql(data)
         procedure_status = mysql_db.execute_proc(sql_statement)
-
-        if procedure_status != 'Success':
+        if procedure_status == 'Success':
             response_status = jsonify(message = "Success"), 200
         else:
             response_status = jsonify({"error": "New user not created", "message": f"{procedure_status}"}), 400
     except Exception as e:
         response_status = jsonify({"error": "New user not created", "message": f"{e}"}), 400
-
     return procedure_status
+
+
+def conncatenate_create_user_sql(data):
+    sql_statement = (f"""CALL PROC_CREATE_USER(
+        '{data["username"]}',
+        '{generate_password_hash(data["password"])}', 
+        '{data["favorite_team"]}', 
+        '{data["notification_preference"]}', 
+        '{data["email_address"]}', 
+        '{data["phone_number"]}', 
+        @status);
+    """)
+    return sql_statement
---
+++ flaskr/user.py
@@ -0,0 +1,43 @@
+from flask import Blueprint, g, jsonify, request, session
+
+from . import mysql_db
+
+bp = Blueprint('user', __name__, url_prefix='/user')
+
+
+@bp.post('/update-email/<str:userID>')
+def update_user_email(data):
+    try:
+        sql_statement = concatenate_update_user_email_sql(data)
+        procedure_status = mysql_db.execute_proc(sql_statement)
+        if procedure_status == 'Status':
+            response_status = jsonify(message = "Success")
+        else:
+            response_status = jsonify({"error": "Email Address not updated", "message": procedure_status})
+    except Exception as e:
+        response_status = jsonify({"error": "Email Address not updated", "message": e}), 400
+
+@bp.post('/update-phone/<str:userID>')
+def update_user_phone(data):
+    try:
+        sql_statement = concatenate_update_user_phone_sql(data)
+        procedure_status = mysql_db.execute_proc(sql_statement)
+        if procedure_status == 'Status':
+            response_status = jsonify(message = "Success")
+        else:
+            response_status = jsonify({"error": "Phone not updated", "message": procedure_status})
+    except Exception as e:
+        response_status = jsonify({"error": "Phone not updated", "message": e}), 400
+
+
+def concatenate_update_user_email_sql(data):
+    user_id = data['userID']
+    email_address = data['emailAddress']
+    sql_statement = f"CALL PROC_UPDATE_USER_EMAIL('{user_id}', '{email_address}', @status);"
+    return sql_statement
+
+def concatenate_update_user_phone_sql(data):
+    user_id = data['userID']
+    phone = data['phone']
+    sql_statement = f"CALL PROC_UPDATE_USER_PHONE('{user_id}', '{phone}', @status);"
+    return sql_statement
