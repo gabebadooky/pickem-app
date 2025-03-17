73a75fc84dedf7febf64f353cc25f2f98294d9d3
gabebadooky <baduquig@gmail.com>
2025-03-17 08:32:59 -0700

    Develop /user/ endpoint and database methods

--- flaskr/user.py
+++ flaskr/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, g, jsonify, request, session
+from flask import Blueprint, jsonify, request
 
 from . import mysql_db
 
@@ -6,56 +6,79 @@ bp = Blueprint('user', __name__, url_prefix='/user')
 
 
 @bp.post('/update-email/<str:userID>')
-def update_user_email(data):
+def update_user_email(data) -> dict:
     try:
-        sql_statement = concatenate_update_user_email_sql(data)
-        procedure_status = mysql_db.execute_proc(sql_statement)
-        if procedure_status == 'Status':
+        procedure_output = sql_update_user_email(data)
+        if procedure_output == 'Status':
             response_status = jsonify(message = "Success")
         else:
-            response_status = jsonify({"error": "Email Address not updated", "message": procedure_status})
+            response_status = jsonify({"error": "Email Address not updated", "message": procedure_output})
     except Exception as e:
         response_status = jsonify({"error": "Email Address not updated", "message": e}), 400
+    return response_status
 
 @bp.post('/update-phone/<str:userID>')
-def update_user_phone(data):
+def update_user_phone(data) -> dict:
     try:
-        sql_statement = concatenate_update_user_phone_sql(data)
-        procedure_status = mysql_db.execute_proc(sql_statement)
-        if procedure_status == 'Status':
+        procedure_output = sql_update_user_phone(data)
+        if procedure_output == 'Status':
             response_status = jsonify(message = "Success")
         else:
-            response_status = jsonify({"error": "Phone not updated", "message": procedure_status})
+            response_status = jsonify({"error": "Phone not updated", "message": procedure_output})
     except Exception as e:
         response_status = jsonify({"error": "Phone not updated", "message": e}), 400
+    return response_status
 
 @bp.post('/update-favorite-team/<str:userID>')
-def update_user_favorite_team(data):
+def update_user_favorite_team(data) -> dict:
     try:
-        sql_statement = (data)
-        procedure_status = mysql_db.execute_proc(sql_statement)
-        if procedure_status == 'Status':
+        procedure_output = sql_update_user_favorite_team(data)
+        if procedure_output == 'Status':
             response_status = jsonify(message = "Success")
         else:
-            response_status = jsonify({"error": "Favorite Team not updated", "message": procedure_status})
+            response_status = jsonify({"error": "Favorite Team not updated", "message": procedure_output})
     except Exception as e:
         response_status = jsonify({"error": "Favorite Team not updated", "message": e}), 400
+    return response_status
+
+@bp.post('/update-notification-preference/<str:userID>')
+def update_user_notification_preference(data) -> dict:
+    try:
+        procedure_output = sql_update_user_notification_preference(data)
+        if procedure_output == 'Status':
+            response_status = jsonify(message = "Success")
+        else:
+            response_status = jsonify({"error": "Notification Preference not updated", "message": procedure_output})
+    except Exception as e:
+        response_status = jsonify({"error": "Notification Preference not updated", "message": e}), 400
+    return response_status
 
 
-def concatenate_update_user_email_sql(data):
+
+def sql_update_user_email(data) -> str:
     user_id = data['userID']
     email_address = data['emailAddress']
     sql_statement = f"CALL PROC_UPDATE_USER_EMAIL('{user_id}', '{email_address}', @status);"
-    return sql_statement
+    procedure_output = mysql_db.execute_proc(sql_statement)
+    return procedure_output
 
-def concatenate_update_user_phone_sql(data):
+def sql_update_user_phone(data) -> str:
     user_id = data['userID']
     phone = data['phone']
     sql_statement = f"CALL PROC_UPDATE_USER_PHONE('{user_id}', '{phone}', @status);"
-    return sql_statement
+    procedure_output = mysql_db.execute_proc(sql_statement)
+    return procedure_output
 
-def concatenate_update_user_favorite_team(data):
+def sql_update_user_favorite_team(data) -> str:
     user_id = data['userID']
     favorite_team = data['favoriteTeam']
-    sql_statement = f"CALL PROC_UPDATE_USER_PHONE('{user_id}', '{favorite_team}', @status);"
-    return sql_statement
+    sql_statement = f"CALL PROC_UPDATE_USER_FAVORITE_TEAM('{user_id}', '{favorite_team}', @status);"
+    procedure_output = mysql_db.execute_proc(sql_statement)
+    return procedure_output
+
+def sql_update_user_notification_preference(data) -> str:
+    user_id = data['userID']
+    favorite_team = data['notificationPreference']
+    sql_statement = f"CALL PROC_UPDATE_USER_NOTIFICATION_PREFERENCE('{user_id}', '{favorite_team}', @status);"
+    procedure_output = mysql_db.execute_proc(sql_statement)
+    return procedure_output
