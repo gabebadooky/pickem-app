120710379ea31296d39ac5131c7208091346cb26
gabebadooky <baduquig@gmail.com>
2025-03-16 21:04:20 -0700

    develop update-favorite-team endpoint and method

--- flaskr/user.py
+++ flaskr/user.py
@@ -29,6 +29,18 @@ def update_user_phone(data):
     except Exception as e:
         response_status = jsonify({"error": "Phone not updated", "message": e}), 400
 
+@bp.post('/update-favorite-team/<str:userID>')
+def update_user_favorite_team(data):
+    try:
+        sql_statement = (data)
+        procedure_status = mysql_db.execute_proc(sql_statement)
+        if procedure_status == 'Status':
+            response_status = jsonify(message = "Success")
+        else:
+            response_status = jsonify({"error": "Favorite Team not updated", "message": procedure_status})
+    except Exception as e:
+        response_status = jsonify({"error": "Favorite Team not updated", "message": e}), 400
+
 
 def concatenate_update_user_email_sql(data):
     user_id = data['userID']
@@ -40,4 +52,10 @@ def concatenate_update_user_phone_sql(data):
     user_id = data['userID']
     phone = data['phone']
     sql_statement = f"CALL PROC_UPDATE_USER_PHONE('{user_id}', '{phone}', @status);"
+    return sql_statement
+
+def concatenate_update_user_favorite_team(data):
+    user_id = data['userID']
+    favorite_team = data['favoriteTeam']
+    sql_statement = f"CALL PROC_UPDATE_USER_PHONE('{user_id}', '{favorite_team}', @status);"
     return sql_statement
