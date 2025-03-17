f7997f6137e82c52d1d17720ff2e210757bbed58
gabebadooky <baduquig@gmail.com>
2025-03-16 18:36:11 -0700

    implement error handling for get-picks endpoint

--- flaskr/picks.py
+++ flaskr/picks.py
@@ -9,13 +9,19 @@ def get_user_picks(username):
     try:
         sql_statement = "SELECT * FROM GET_PICKS_VW WHERE USERNAME = '{username}';"
         picks = mysql_db.call_view(sql_statement)
+
+        if len(picks) == 0:
+            response_status = jsonify({"error": "Not Found", "message": "No picks found associated to the provided username."}), 406
+        else:
+            response_status = jsonify({
+                'username': picks['username'],
+                'gameID': picks['game_id'],
+                'teamPicked': picks['team_picked'],
+                'pickWeight': picks['pick_weight']
+            }), 200
+    
     except Exception as e:
         response = jsonify({"error": "Request Error", "message": f"{e}"})
-        return response 400
-    
-    if len(picks) == 0:
-        response = jsonify({"error": "Not Found", "message": "No picks found associated to the provided username."})
-        return response, 406
-    else:
-        response = jsonify("picks": picks)
-        return response, 200
+        response_status = response, 400
+        
+    return response_status
