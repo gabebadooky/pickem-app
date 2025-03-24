bb1f070b32a57ed68c05528b071426e67bf6e1e0
Gabriel Baduqui <baduquig@gmail.com>
2025-03-23 17:07:37 -0700

    Develop endpoint and database method to update pick

--- flaskr/picks.py
+++ flaskr/picks.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, g, jsonify, request, session
+from flask import Blueprint, jsonify, request
 
 from . import mysql_db
 
@@ -7,7 +7,7 @@ bp = Blueprint('picks', __name__, url_prefix='/picks')
 @bp.get('/<str:username>')
 def get_user_picks(username):
     try:
-        sql_statement = "SELECT * FROM GET_PICKS_VW WHERE USERNAME = '{username}';"
+        sql_statement = f"SELECT * FROM GET_PICKS_VW WHERE USERNAME = '{username}';"
         picks = mysql_db.call_view(sql_statement)
 
         if len(picks) == 0:
@@ -25,3 +25,28 @@ def get_user_picks(username):
         response_status = response, 400
         
     return response_status
+
+
+@bp.post('/submit')
+def submit_pick():
+    data = request.json
+    try:
+        procedure_output = sql_update_pick(data)
+        if procedure_output == 'Success':
+            response_status = jsonify(message = "Success"), 201
+        else:
+            response_status = jsonify({"error": f"Pick {data['pick']} not updated", "message": procedure_output}), 400
+    except Exception as e:
+        response_status = jsonify({"error": f"Pick {data['pick']} not updated", "message": procedure_output}), 400
+    return response_status
+
+
+
+def sql_update_pick(data) -> str:
+    user_id = data['userID']
+    game_id = data['gameID']
+    team_picked = data['teamPicked']
+    pick_weight = data['pickWeight']
+    sql_statement = f"CALL PROC_SUBMIT_PICK('{user_id}', '{game_id}', '{team_picked}', '{pick_weight}', @status);"
+    procedure_output = mysql_db.execute_proc(sql_statement)
+    return procedure_output
