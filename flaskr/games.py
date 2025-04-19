5d7143e027a0a793e0b058c856c6b0dfa3586166
Gabriel Baduqui <baduquig@gmail.com>
2025-04-19 15:19:21 -0700

    picks component WIP...

--- flaskr/games.py
+++ flaskr/games.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify
 from . import mysql_db
 
 bp = Blueprint('games', __name__, url_prefix='/games')
 
-@bp.get('/games')
+@bp.get('/')
 def get_games() -> tuple:
     try:
         sql_statement = f"SELECT * FROM GET_GAMES_VW;"
@@ -19,24 +19,62 @@ def get_games() -> tuple:
                     "league": games[x]["LEAGUE"],
                     "week": games[x]["WEEK"],
                     "year": games[x]["YEAR"],
-                    "cbsCode": games[x]["CBS_CODE"],
-                    "espnCode": games[x]["ESPN_CODE"],
-                    "foxCode": games[x]["FOX_CODE"],
-                    "vegasCode": games[x]["VEGAS_CODE"],
                     "awayTeamID": games[x]["AWAY_TEAM_ID"],
                     "homeTeamID": games[x]["HOME_TEAM_ID"],
                     "date": games[x]["DATE"],
-                    "time": games[x]["TIME"],
+                    "time": str(games[x]["TIME"]),
                     "tvCoverage": games[x]["TV_COVERAGE"],
                     "stadium": games[x]["STADIUM"],
                     "city": games[x]["CITY"],
                     "state": games[x]["STATE"],
-                    "gameFinished": games[x]["GAME_FINISHED"]
+                    "gameFinished": games[x]["GAME_FINISHED"],
+                    #"cbsCode": games[x]["CBS_CODE"],
+                    "cbsAwayMoneyline": games[x]["CBS_AWAY_MONEYLINE"],
+                    "cbsHomeMoneyline": games[x]["CBS_HOME_MONEYLINE"],
+                    "cbsAwaySpread": games[x]["CBS_AWAY_SPREAD"],
+                    "cbsHomeSpread": games[x]["CBS_HOME_SPREAD"],
+                    "cbsOverUnder": games[x]["CBS_OVER_UNDER"],
+                    "cbsAwayWinPercentage": games[x]["CBS_AWAY_WIN_PERCENTAGE"],
+                    #"espnCode": games[x]["ESPN_CODE"],
+                    "espnAwayMoneyline": games[x]["ESPN_AWAY_MONEYLINE"],
+                    "espnHomeMoneyline": games[x]["ESPN_HOME_MONEYLINE"],
+                    "espnAwaySpread": games[x]["ESPN_AWAY_SPREAD"],
+                    "espnHomeSpread": games[x]["ESPN_HOME_SPREAD"],
+                    "espnOverUnder": games[x]["ESPN_OVER_UNDER"],
+                    "espnAwayWinPercentage": games[x]["ESPN_AWAY_WIN_PERCENTAGE"],
+                    #"foxCode": games[x]["FOX_CODE"],
+                    "foxAwayMoneyline": games[x]["FOX_AWAY_MONEYLINE"],
+                    "foxHomeMoneyline": games[x]["FOX_HOME_MONEYLINE"],
+                    "foxAwaySpread": games[x]["FOX_AWAY_SPREAD"],
+                    "foxHomeSpread": games[x]["FOX_HOME_SPREAD"],
+                    "foxOverUnder": games[x]["FOX_OVER_UNDER"],
+                    "foxAwayWinPercentage": games[x]["FOX_AWAY_WIN_PERCENTAGE"],
+                    #"vegasCode": games[x]["VEGAS_CODE"],
+                    "vegasAwayMoneyline": games[x]["VEGAS_AWAY_MONEYLINE"],
+                    "vegasHomeMoneyline": games[x]["VEGAS_HOME_MONEYLINE"],
+                    "vegasAwaySpread": games[x]["VEGAS_AWAY_SPREAD"],
+                    "vegasHomeSpread": games[x]["VEGAS_HOME_SPREAD"],
+                    "vegasOverUnder": games[x]["VEGAS_OVER_UNDER"],
+                    "vegasAwayWinPercentage": games[x]["VEGAS_AWAY_WIN_PERCENTAGE"],
+                    "awayQ1BoxScore": games[x]["AWAY_Q1_BOX_SCORE"],
+                    "awayQ2BoxScore": games[x]["AWAY_Q2_BOX_SCORE"],
+                    "awayQ3BoxScore": games[x]["AWAY_Q3_BOX_SCORE"],
+                    "awayQ4BoxScore": games[x]["AWAY_Q4_BOX_SCORE"],
+                    "awayOvertimeBoxScore": games[x]["AWAY_OVERTIME_BOX_SCORE"],
+                    "awayTotalBoxScore": games[x]["AWAY_TOTAL_BOX_SCORE"],
+
+                    "homeQ1BoxScore": games[x]["HOME_Q1_BOX_SCORE"],
+                    "homeQ2BoxScore": games[x]["HOME_Q2_BOX_SCORE"],
+                    "homeQ3BoxScore": games[x]["HOME_Q3_BOX_SCORE"],
+                    "homeQ4BoxScore": games[x]["HOME_Q4_BOX_SCORE"],
+                    "homeOvertimeBoxScore": games[x]["HOME_OVERTIME_BOX_SCORE"],
+                    "homeTotalBoxScore": games[x]["HOME_TOTAL_BOX_SCORE"]
                 }
                 camel_cased_list.append(camel_cased_pick)
             response_status = jsonify(camel_cased_list), 200
     except Exception as e:
-        response = jsonify({"error": "Request Error", "message": e})
+        print(f"Request Error: {e}")
+        response = jsonify({"error": "Request Error", "message": f"{e}"})
         response_status = response, 400
     
     return response_status
--- flaskr/teams.py
+++ flaskr/teams.py
@@ -1,39 +1,45 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify
 from . import mysql_db
 
-bp = Blueprint('teams', __name__, url_prefix='/games')
+bp = Blueprint('teams', __name__, url_prefix='/teams')
 
-@bp.get('/teams')
-def get_games() -> tuple:
+@bp.get('/')
+def get_teams() -> tuple:
     try:
         sql_statement = f"SELECT * FROM GET_TEAMS_VW;"
-        games = mysql_db.call_view(sql_statement)
+        teams = mysql_db.call_view(sql_statement)
 
-        if len(games) == 0:
+        if len(teams) == 0:
             response_status = jsonify({"error": "Not Found", "message": f"No records retrieved from given query:\n{sql_statement}"}), 400
         else:
             camel_cased_list = []
-            for x in range(len(games)):
+            for x in range(len(teams)):
                 camel_cased_pick = {
-                    "teamID": games[x]["TEAM_ID"],
-                    "cbsCode": games[x]["CBS_CODE"],
-                    "espnCode": games[x]["ESPN_CODE"],
-                    "foxCode": games[x]["FOX_CODE"],
-                    "vegasCode": games[x]["VEGAS_CODE"],
-                    "conferenceCode": games[x]["CONFERENCE_CODE"],
-                    "conferenceName": games[x]["CONFERENCE_NAME"],
-                    "divisionName": games[x]["DIVISION_NAME"],
-                    "teamName": games[x]["TEAM_NAME"],
-                    "teamMascot": games[x]["TEAM_MASCOT"],
-                    "powerConference": games[x]["POWER_CONFERENCE"],
-                    "team_logo_url": games[x]["TEAM_LOGO_URL"],
-                    "primaryColor": games[x]["PRIMARY_COLOR"],
-                    "alternateColor": games[x]["ALTERNATE_COLOR"]
+                    "teamID": teams[x]["TEAM_ID"],
+                    "cbsCode": teams[x]["CBS_CODE"],
+                    "espnCode": teams[x]["ESPN_CODE"],
+                    "foxCode": teams[x]["FOX_CODE"],
+                    "vegasCode": teams[x]["VEGAS_CODE"],
+                    "conferenceCode": teams[x]["CONFERENCE_CODE"],
+                    "conferenceName": teams[x]["CONFERENCE_NAME"],
+                    "divisionName": teams[x]["DIVISION_NAME"],
+                    "teamName": teams[x]["TEAM_NAME"],
+                    "teamMascot": teams[x]["TEAM_MASCOT"],
+                    "powerConference": teams[x]["POWER_CONFERENCE"],
+                    "team_logo_url": teams[x]["TEAM_LOGO_URL"],
+                    "primaryColor": teams[x]["PRIMARY_COLOR"],
+                    "alternateColor": teams[x]["ALTERNATE_COLOR"],
+                    "overallWins": teams[x]["OVERALL_WINS"],
+                    "overallLosses": teams[x]["OVERALL_LOSSES"],
+                    "overallTies": teams[x]["OVERALL_TIES"],
+                    "conferenceWins": teams[x]["CONFERENCE_WINS"],
+                    "conferenceLosses": teams[x]["CONFERENCE_LOSSES"],
+                    "conferenceTies": teams[x]["CONFERENCE_TIES"],
                 }
                 camel_cased_list.append(camel_cased_pick)
             response_status = jsonify(camel_cased_list), 200
     except Exception as e:
-        response = jsonify({"error": "Request Error", "message": e})
+        response = jsonify({"error": "Request Error", "message": f"{e}"})
         response_status = response, 400
     
     return response_status
