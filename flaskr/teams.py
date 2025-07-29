6113c689fb21f0ef9082221ef84d6a399348cd99
gbaduqui <baduquig@gmail.com>
2025-07-28 21:25:45 -0700

    b64 encoding WIP...

--- flaskr/teams.py
+++ flaskr/teams.py
@@ -101,10 +101,9 @@ def update_team_notes() -> tuple:
 
 
 def sql_update_team_notes(data: dict) -> str:
-    notes_str: str = data["notes"]
     user_id: int = data["userID"]
     team_id: str = data["teamID"]
-    notes: bytes = base64.b64encode(notes_str.encode("utf-8"))
-    sql_statement: str = f"CALL PROC_UPDATE_TEAM_NOTES({user_id}, '{team_id}', {notes}, @status);"
+    notes: bytes = data["notes"]
+    sql_statement: str = f"CALL PROC_UPDATE_TEAM_NOTES({user_id}, '{team_id}', '{notes}', @status);"
     procedure_output: str = mysql_db.execute_proc(sql_statement)
     return procedure_output
