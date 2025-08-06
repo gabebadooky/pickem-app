46f2ffb720ea9720a19171421eb13a24eb341811
gbaduqui <baduquig@gmail.com>
2025-08-06 11:55:42 -0700

    Google OAuth WIP...

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -71,7 +71,7 @@ def login() -> tuple:
 def login_google() -> tuple:
     try:
         redirect_uri = url_for("auth.authorize_google", _external=True)
-        return google.authorize_redirect(redirect_uri)
+        google.authorize_redirect(redirect_uri)
     except Exception as e:
         print(f"Error occurred during Google OAuth Login attempt: {e}")
         return jsonify({"error": "Google Login Error", "message": f"{e}"}), 400
