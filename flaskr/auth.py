9f95fad1b20fa66a354b16caf5aab4fb045020f5
gbaduqui <baduquig@gmail.com>
2025-08-06 11:53:46 -0700

    Google OAuth WIP...

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -88,10 +88,11 @@ def authorize_google():
     user: dict = mysql_db.get_user_by_username(username)
     if user is None:
         mysql_db.execute_proc(concatenate_create_user_sql(user_info))
-        #response_status: tuple = jsonify(token), 200        
-    #else:
-        #response_status: tuple = jsonify(token), 200
-    return redirect(f"https://have-a-nice-pickem.onrender.com?access_token={token}")
+        response_status: tuple = jsonify(token), 200        
+    else:
+        response_status: tuple = jsonify(token), 200
+    redirect(f"https://have-a-nice-pickem.onrender.com") #?access_token={token}")
+    return response_status
 ### OAUTH ###
 
 
