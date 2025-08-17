dc053388d99acb9d40bd59fd2a50584b0f87f645
Gabriel Baduqui <baduquig@gmail.com>
2025-08-17 14:18:03 -0700

    develop /maintenance endpoint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,5 @@
 import os
-from . import auth, games, teams, picks, user
+from . import auth, games, maintenance, picks, teams, user
 from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
@@ -37,6 +37,7 @@ def create_app(test_config=None):
     app.register_blueprint(teams.bp)
     app.register_blueprint(picks.bp)
     app.register_blueprint(user.bp)
+    app.register_blueprint(maintenance.bp)
 
     @app.route('/')
     def hello():
---
+++ flaskr/maintenance.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify
+from . import mysql_db
+
+bp: Blueprint = Blueprint("maintenance", __name__, url_prefix="/maintenance")
+
+@bp.get("/")
+def call_is_system_under_maintenance_view() -> tuple:
+    try:
+        is_system_under_maintenance: int = mysql_db.call_view("SELECT * FROM IS_SYSTEM_UNDER_MAINTENANCE_VW WHERE ROWNUM = 1")
+        response_status = jsonify({"underMaintenance": is_system_under_maintenance["UNDER_MAINTENANCE"]}), 200
+    except Exception as e:
+        response: dict = jsonify({"error": "Request Error", "message": f"{e}"})
+        response_status: tuple = response, 400
+    return response_status
