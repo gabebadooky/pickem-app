7717dd0972dae9558b817237ccec837f3730fdbc
Gabriel Baduqui <baduquig@gmail.com>
2025-08-17 18:28:32 -0700

    fix /maintenance endpoint query

--- flaskr/maintenance.py
+++ flaskr/maintenance.py
@@ -6,7 +6,7 @@ bp: Blueprint = Blueprint("maintenance", __name__, url_prefix="/maintenance")
 @bp.get("/")
 def call_is_system_under_maintenance_view() -> tuple:
     try:
-        is_system_under_maintenance: int = mysql_db.call_view("SELECT * FROM IS_SYSTEM_UNDER_MAINTENANCE_VW WHERE ROWNUM = 1")
+        is_system_under_maintenance: int = mysql_db.call_view("SELECT * FROM IS_SYSTEM_UNDER_MAINTENANCE_VW;")
         response_status = jsonify({"underMaintenance": is_system_under_maintenance["UNDER_MAINTENANCE"]}), 200
     except Exception as e:
         response: dict = jsonify({"error": "Request Error", "message": f"{e}"})
