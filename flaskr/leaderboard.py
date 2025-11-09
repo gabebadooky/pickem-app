4ba173b4753c9b578a80c4f41e2953c1849e93f5
Gabriel Baduqui <baduquig@gmail.com>
2025-11-09 11:02:42 -0700

    develop /leaderboard/<week> and /leaderboard/season endpoints, calling new leaderboard_vw view

--- flaskr/leaderboard.py
+++ flaskr/leaderboard.py
@@ -5,6 +5,69 @@ from . import mysql_db
 bp: Blueprint = Blueprint("leaderboard", __name__, url_prefix="/leaderboard")
 
 
+@bp.get("/<week>")
+def get_week_leaderboard(week) -> tuple:
+    try:
+        sql_statement: str = f"SELECT * FROM LEADERBOAD_VW WHERE WEEK = {week};"
+        leaderboard: list = mysql_db.call_view(sql_statement)
+
+        camel_cased_list: list = []
+        for x in range(len(leaderboard)):
+            camel_cased_row: dict = {
+                "userID": leaderboard[x]["USER_ID"],
+                "displayName": leaderboard[x]["DISPLAY_NAME"],
+                "year": leaderboard[x]["YEAR"],
+                "week": leaderboard[x]["WEEK"],
+                "league": leaderboard[x]["LEAGUE"],
+                "top25": leaderboard[x]["TOP_25_MATCHUP"],
+                "powerConference": leaderboard[x]["P4_MATCHUP"],
+                "nonPowerConference": leaderboard[x]["G6_MATCHUP"],
+                "totalPoints": leaderboard[x]["TOTAL_POINTS"],
+                "correctPicks": leaderboard[x]["CORRECT_PICKS"],
+                "incorrectPicks": leaderboard[x]["INCORRECT_PICKS"],
+                "picksSubmitted": leaderboard[x]["PICKS_SUBMITTED"]
+            }
+            camel_cased_list.append(camel_cased_row)
+        response_status: tuple = jsonify(camel_cased_list), 200
+    except Exception as e:
+        response: dict = jsonify({"error": "Request Error", "message": f"{e}"})
+        response_status: tuple = response, 400
+    
+    return response_status
+
+
+@bp.get("/season")
+def get_week_leaderboard(week) -> tuple:
+    try:
+        sql_statement: str = f"SELECT * FROM LEADERBOAD_VW;"
+        leaderboard: list = mysql_db.call_view(sql_statement)
+
+        camel_cased_list: list = []
+        for x in range(len(leaderboard)):
+            camel_cased_row: dict = {
+                "userID": leaderboard[x]["USER_ID"],
+                "displayName": leaderboard[x]["DISPLAY_NAME"],
+                "year": leaderboard[x]["YEAR"],
+                "week": leaderboard[x]["WEEK"],
+                "league": leaderboard[x]["LEAGUE"],
+                "top25": leaderboard[x]["TOP_25_MATCHUP"],
+                "powerConference": leaderboard[x]["P4_MATCHUP"],
+                "nonPowerConference": leaderboard[x]["G6_MATCHUP"],
+                "totalPoints": leaderboard[x]["TOTAL_POINTS"],
+                "correctPicks": leaderboard[x]["CORRECT_PICKS"],
+                "incorrectPicks": leaderboard[x]["INCORRECT_PICKS"],
+                "picksSubmitted": leaderboard[x]["PICKS_SUBMITTED"]
+            }
+            camel_cased_list.append(camel_cased_row)
+        response_status: tuple = jsonify(camel_cased_list), 200
+    except Exception as e:
+        response: dict = jsonify({"error": "Request Error", "message": f"{e}"})
+        response_status: tuple = response, 400
+    
+    return response_status
+
+
+
 @bp.get("/")
 def get_leaderboard() -> tuple:
     try:
